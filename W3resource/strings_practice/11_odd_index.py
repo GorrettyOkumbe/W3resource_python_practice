b81e583cff602773f9195273c3033b39e0e18456
OPunga <ronnyopunga96@gmail.com>
2022-09-24 23:18:28 +0300

    remove odd value index

--- W3resource/strings_practice/11_odd_index.py
+++ W3resource/strings_practice/11_odd_index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def odd_ind(str):
+def odd_ind(my_string):
     var1 = ""
-    for x in range(len(str)):
+    for x in range(len(my_string)):
         if x % 2 == 0:
-            var1 += str[x]
+            var1 += my_string[x]
     return var1
 
 print(odd_ind("python"))
