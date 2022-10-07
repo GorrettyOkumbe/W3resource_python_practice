aaf9528b4cc6d59d88923c66ee5fb08f832406fb
“[Careen <careennaitore@gmail.com>
2022-10-07 15:48:20 +0300

    object

--- W3resource/python_classes/3_object_display.py
+++ W3resource/python_classes/3_object_display.py
@@ -7,15 +7,19 @@ import json
 
 class Employee:
     count = 0
-    #args = sys.argv
-    def __init__(self, employee_name, employee_age):
-        self.name = employee_name
-        self.age = employee_age
+    def __init__(self, name, age, args=[]):
+        self.name = name
+        self.age = age
+        self.args = sys.argv[1:]
+        try:
+            self.email = self.args[0]
+            self.salary = self.args[1]
+        except Exception as e:
+            print("number of args is insufficient")
+
         Employee.count += 1
-
-n = len(sys.argv)
-for x in range(1, n):
-    print(sys.argv[x])
-
-
+employee = Employee("sam", 34)
 #print(Employee.count)
+print(employee.__dict__)
+#print(employee.salary)
+
